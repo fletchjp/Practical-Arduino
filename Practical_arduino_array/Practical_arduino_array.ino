7dde362025b997da0c2725d49e45c6871303779e
Sven Rosvall <sven@rosvall.ie>
2021-07-26 22:34:03 +0100

    Introduce struct ServoTurnout.

--- Practical_arduino_array/Practical_arduino_array.ino
+++ Practical_arduino_array/Practical_arduino_array.ino
@@ -8,34 +8,43 @@ const byte NUMBER_OF_ROUTE_BUTTONS = 2;
 const byte NUMBER_OF_TURNOUTS = 5;
 #define TURNOUT_MOVE_SPEED 30              // [ms] lower number is faster
 
+struct ServoTurnout
+{
+  int pin;
+  int closedPosition;
+  int thrownPosition;
+  Servo servo;
+  int targetPosition;  // renamed from turnoutTarget
+  int currentPosition; // renamed from turnoutPosition
+  unsigned long moveDelay;
+  bool isClosed;
+};
 
-/* assign servos in an array */
-Servo turnoutServo[NUMBER_OF_TURNOUTS];                     // Define servo array
+// define the turnouts with their pins and end position angles.
+ServoTurnout turnouts[NUMBER_OF_TURNOUTS] = {
+  {2, 50, 90},
+  {3, 80, 150},
+  {4, 60, 120},
+  {5, 60, 120},
+  {6, 60, 120}
+};
 
 /* Setup servo arrays  */
 int pushButtonPins[NUMBER_OF_PUSH_BUTTONS] = {7, 8, 9, 10}; // enter pin numbers. 1st number is for pushButtonPins[0], 2nd for pushButtonPins[1] etc
 int routeButtonPins[NUMBER_OF_ROUTE_BUTTONS] = {11, 12};    // enter pin numbers. 1st number is for routeButtonPins[0], 2nd for routeButtonPins[1] etc
-int turnoutPins[NUMBER_OF_TURNOUTS] = {2, 3, 4, 5, 6};      // eWnter pin numbers. 1st number is for allTurnouts[0], 2nd for allTurnouts[1] etc
-int turnoutTarget[NUMBER_OF_TURNOUTS] ;                     // the angle you want the servo to turn to, will be set to either min or max
-int turnoutPosition[NUMBER_OF_TURNOUTS] ;                   // current angle of servo
-unsigned long turnoutMoveDelay[NUMBER_OF_TURNOUTS];         // valuse to compare with millis() to check if it is time to move a turnoutServo
-
-int turnoutClosedPosition[] = {50, 80,  60,  60,  60};      // Turnout closed angle. 1st number is for allTurnouts[0], 2nd for allTurnouts[1] etc.
-int turnoutThrownPosition[] = {90, 150, 120, 120, 120};     // Turnout thrown angle. 1st number is for allTurnouts[0], 2nd for allTurnouts[1] etc.
-bool turnoutIsClosed[NUMBER_OF_TURNOUTS];                   // turnout state
 
 void throwTurnout(int i){
-  turnoutMoveDelay[i] = millis() + TURNOUT_MOVE_SPEED;
-  if (turnoutPosition[i] < turnoutTarget[i]) turnoutPosition[i]++;
-  if (turnoutPosition[i] > turnoutTarget[i]) turnoutPosition[i]--;
-  turnoutServo[i].write(turnoutPosition[i]);
-  if (turnoutPosition[i] == turnoutClosedPosition[i]) {
-    turnoutIsClosed[i] = true;
-    if (i == 3) turnoutTarget[i+1] = turnoutClosedPosition[i+1];
+  turnouts[i].moveDelay = millis() + TURNOUT_MOVE_SPEED;
+  if (turnouts[i].currentPosition < turnouts[i].targetPosition) turnouts[i].currentPosition++;
+  if (turnouts[i].currentPosition > turnouts[i].targetPosition) turnouts[i].currentPosition--;
+  turnouts[i].servo.write(turnouts[i].currentPosition);
+  if (turnouts[i].currentPosition == turnouts[i].closedPosition) {
+    turnouts[i].isClosed = true;
+    if (i == 3) turnouts[i+1].targetPosition = turnouts[i+1].closedPosition;
   }
-  if (turnoutPosition[i] == turnoutThrownPosition[i]) {
-    turnoutIsClosed[i] = false;
-    if (i == 3) turnoutTarget[i+1] = turnoutThrownPosition[i+1];
+  if (turnouts[i].currentPosition == turnouts[i].thrownPosition) {
+    turnouts[i].isClosed = false;
+    if (i == 3) turnouts[i+1].targetPosition = turnouts[i+1].thrownPosition;
   }
 }
 
@@ -43,45 +52,45 @@ void setup() {
   Serial.begin(9600);
   /* setup all the servos*/
   for (int thisTurnout = 0; thisTurnout < NUMBER_OF_TURNOUTS; thisTurnout++) {
-      turnoutTarget[thisTurnout]   = turnoutClosedPosition[thisTurnout];      // setup targets as closed positiion
-      turnoutPosition[thisTurnout] = turnoutClosedPosition[thisTurnout];      // setup position to closed
-      turnoutIsClosed[thisTurnout] = true;                                    // setup closed is true
+      turnouts[thisTurnout].targetPosition  = turnouts[thisTurnout].closedPosition; // setup targets as closed positiion
+      turnouts[thisTurnout].currentPosition = turnouts[thisTurnout].closedPosition; // setup position to closed
+      turnouts[thisTurnout].isClosed = true;                                        // setup closed is true
       
-      turnoutServo[thisTurnout].attach(turnoutPins[thisTurnout]);             // attach all the turnout servos
-      turnoutServo[thisTurnout].write(turnoutPosition[thisTurnout]);          // write the servos poistions
+      turnouts[thisTurnout].servo.attach(turnouts[thisTurnout].pin);                // attach all the turnout servos
+      turnouts[thisTurnout].servo.write(turnouts[thisTurnout].currentPosition);     // write the servos poistions
   }
   
   /* setup all the pushbuttons */
-  for (int i = 0; i < NUMBER_OF_PUSH_BUTTONS;  i++) pinMode(pushButtonPins[i], INPUT_PULLUP);      // initailise all the push buttons with pullups 
-  for (int i = 0; i < NUMBER_OF_ROUTE_BUTTONS; i++) pinMode(routeButtonPins[i], INPUT_PULLUP);     // initailise all the route buttons with pullups 
+  for (int i = 0; i < NUMBER_OF_PUSH_BUTTONS;  i++) pinMode(pushButtonPins[i], INPUT_PULLUP);  // initailise all the push buttons with pullups 
+  for (int i = 0; i < NUMBER_OF_ROUTE_BUTTONS; i++) pinMode(routeButtonPins[i], INPUT_PULLUP); // initailise all the route buttons with pullups 
 
 }
 
 void loop() {
   /* loop through all the push buttons */
   for (int i = 0; i < NUMBER_OF_PUSH_BUTTONS; i++){
-    if (digitalRead(pushButtonPins[i]) == LOW){                               // is push button pressed
-      if (turnoutIsClosed[i])  turnoutTarget[i] = turnoutThrownPosition[i];   // set turnout target to thrown if it is closed
-      if (!turnoutIsClosed[i]) turnoutTarget[i] = turnoutClosedPosition[i];   // set turnout target to closed if it is thrown
+    if (digitalRead(pushButtonPins[i]) == LOW){                                           // is push button pressed
+      if (turnouts[i].isClosed)  turnouts[i].targetPosition = turnouts[i].thrownPosition; // set turnout target to thrown if it is closed
+      if (!turnouts[i].isClosed) turnouts[i].targetPosition = turnouts[i].closedPosition; // set turnout target to closed if it is thrown
     }
   }
   
   /* read route buttons and move turnouts when pressed */
-  if (digitalRead(routeButtonPins[0]) == LOW){                              // is route button pressed
-    if (!turnoutIsClosed[0]) turnoutTarget[0] = turnoutClosedPosition[0];   // set turnout target to closed if it is thrown
-    if (!turnoutIsClosed[1]) turnoutTarget[1] = turnoutClosedPosition[1];   // set turnout target to closed if it is thrown
+  if (digitalRead(routeButtonPins[0]) == LOW){                                          // is route button pressed
+    if (!turnouts[0].isClosed) turnouts[0].targetPosition = turnouts[0].closedPosition; // set turnout target to closed if it is thrown
+    if (!turnouts[1].isClosed) turnouts[1].targetPosition = turnouts[1].closedPosition; // set turnout target to closed if it is thrown
   }
-  if (digitalRead(routeButtonPins[1]) == LOW){                           // is route button pressed
-    if (turnoutIsClosed[0]) turnoutTarget[0] = turnoutThrownPosition[0];   // set turnout target to thrown if it is thrown
-    if (turnoutIsClosed[1]) turnoutTarget[1] = turnoutThrownPosition[1];   // set turnout target to thrown if it is thrown
+  if (digitalRead(routeButtonPins[1]) == LOW){                                          // is route button pressed
+    if (turnouts[0].isClosed) turnouts[0].targetPosition = turnouts[0].thrownPosition;  // set turnout target to thrown if it is thrown
+    if (turnouts[1].isClosed) turnouts[1].targetPosition = turnouts[1].thrownPosition;  // set turnout target to thrown if it is thrown
   }
 
   
   /* loop through the turnout servos */
   for (int thisTurnout = 0; thisTurnout < NUMBER_OF_TURNOUTS; thisTurnout++) {
-    if (turnoutPosition[thisTurnout] != turnoutTarget[thisTurnout]) {           // check that they are not at there taregt position
-      if (millis() > turnoutMoveDelay[thisTurnout]) {                           // check that enough delay has passed
-           throwTurnout(thisTurnout);                                          // call throwTurnout to move the servo 1 degree towards its target
+    if (turnouts[thisTurnout].currentPosition != turnouts[thisTurnout].targetPosition) { // check that they are not at there taregt position
+      if (millis() > turnouts[thisTurnout].moveDelay) {                                  // check that enough delay has passed
+           throwTurnout(thisTurnout);                                                    // call throwTurnout to move the servo 1 degree towards its target
       }
     }
   }
